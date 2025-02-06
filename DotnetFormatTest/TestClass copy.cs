cfd07f4e1ee506743dd5d1aa25e447753fdf1963
Wouter Mooij <wouter@1twaalf.nl>
2025-02-06 06:42:01 +0100

    Rename TestClass to TestClassCopy and fix message formatting in both TestClass files

--- DotnetFormatTest/TestClass copy.cs
+++ DotnetFormatTest/TestClass copy.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DotnetFormatTest;
 
-public class TestClass
+public class TestClassCopy
 {
     public string GetMessage()
     {
-        return "Hello World!!s!s";
+        return "Hello World!";
     }
 }
--- DotnetFormatTest/TestClass.cs
+++ DotnetFormatTest/TestClass.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DotnetFormatTest;
 
 public class TestClass
 {
     public string GetMessage()
     {
-        return "Helsslo World!!d!s";
+        return "Hello World!";
     }
 }
