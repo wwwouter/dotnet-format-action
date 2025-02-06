e8aa746a762217a5217ec91211e279a974c32fa2
GitHub Action <action@github.com>
2025-02-06 05:42:32 +0000

    Apply dotnet format formatting

--- DotnetFormatTest/TestClass.cs
+++ DotnetFormatTest/TestClass.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DotnetFormatTest;
 
 public class TestClass
