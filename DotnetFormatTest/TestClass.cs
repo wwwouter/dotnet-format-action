230a4844dd2d56871de789a14ceace16ae24df83
Wouter Mooij <wouter@1twaalf.nl>
2025-02-06 06:27:02 +0100

    Fix typo in GetMessage method in TestClass

--- DotnetFormatTest/TestClass.cs
+++ DotnetFormatTest/TestClass.cs
@@ -1,9 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DotnetFormatTest;
 
 public class TestClass
 {
     public string GetMessage()
     {
-        return "Hello World!!d!s";
+        return "Helslo World!!d!s";
     }
 }
