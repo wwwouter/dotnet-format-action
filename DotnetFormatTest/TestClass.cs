6a0b629b67724c2da3df5076a6bc0783994d3d9e
Wouter Mooij <wouter@1twaalf.nl>
2025-02-06 06:09:22 +0100

    Fix message output in TestClass to correct greeting text

--- DotnetFormatTest/TestClass.cs
+++ DotnetFormatTest/TestClass.cs
@@ -1,9 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DotnetFormatTest;
 
 public class TestClass
 {
     public string GetMessage()
     {
-        return "Hello World!!!s";
+        return "Hello World!!d!s";
     }
 }
